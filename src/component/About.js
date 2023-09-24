6d4daefd50dee7bdc95297a6e3f1570c7848f4c0
Chamsron <emmacameron219@gmail.com>
2023-09-24 17:18:43 +0100

    About page now has image

--- src/component/About.js
+++ src/component/About.js
@@ -1,15 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+import ECam from '../images/ECam.png'
+
+
+const AboutHeader = styled.h2`
+    text-align:center;
+    font-family: roboto;
+    font-size: 75px;
+`
+const AboutSubHeading = styled.h3`
+    text-align:center;
+    font-family: roboto;
+    font-size: 40px;
+`
+
+const AboutSubSubHeading = styled.h4`
+    text-align:center;
+    font-family: roboto;
+    font-size: 30px;
+`
+
+const Paragraph = styled.p`
+    text-align:center;
+    font-family: roboto;
+    font-size: 20px;
+`
+
+const ImageContainer = styled.body`
+    align-self:center;
+    display:flex;
+    height:50vh;
+    width:100vw;
+    justify-content:center;
+`
 
 const About = () => {
     return ( 
         <div className='body'>
-            <h2>Welcome to the about page</h2>
-            <p>
-                Why coding?
-                <br></br>
+            <AboutHeader>About Me</AboutHeader>
+            <ImageContainer>
+                <img src={ECam}/>
+            </ImageContainer>
+
+            <AboutSubSubHeading>A small introduction about myself</AboutSubSubHeading>
+
+            <AboutSubHeading>Emma Cameron</AboutSubHeading>
+
+            <AboutSubSubHeading>Junior Software Engingeer</AboutSubSubHeading>
+
+            <Paragraph>
                 After working in IT Customer Support for a number of years, I wanted to take the leap to learn code; I wanted to go from listening to users problems and experiences using the product to fixing pre-existing applications and creating new applications.
                 
-            </p>
+            </Paragraph>
         </div>
      );
 }
--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import "react-vertical-timeline-component/style.min.css";
+import Brief from '../images/kendobrief.png';
+import Timeline from '../images/kendomodels.png';
+import Proud from '../images/proudof.png';
+import Proud2 from '../images/hardwonvictory.png';
 
 const PortfolioPage = () => {
+
     return ( 
         <div>
             <VerticalTimeline lineColor='#FF6969'>
@@ -13,7 +18,16 @@ const PortfolioPage = () => {
                 <p>I signed up for codecademy's Learn SQL course to learn the basics of managing large datasets as well as analysing data.</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement className='vertical-timeline-element--work' date='CodeClan Week 5' iconStyle={{background:"#FF6969", color: "#141E46"}}>
-                <h4>Solo Python Project</h4>
+                <h4>CodeClan Solo Python Project</h4>
+                <p>After 5 weeks of learning python, I was given my solo-python project.</p>
+                <br></br>
+                <p>I was tasked with building a score keeping application for your sport of choice. As I am an avid Kendo enthusiast, I tailored the brief to reflect this.</p>
+                <br></br>
+                <img className="ExamplePhotos" src={Brief}/>
+                <img className="ExamplePhotos" src={Timeline}/>
+                <img className="ExamplePhotos" src={Proud}/>
+                <img className="ExamplePhotos" src={Proud2}/>
+                <br></br>
                 </VerticalTimelineElement>
             </VerticalTimeline>
         </div>
